2791603c0ae6e21aa5e3c118f2e539bdc404c3cb
Anx0Fdez <afernandezrodriguez@danielcastelao.org>
2023-11-20 09:11:17 +0100

    docs.v1

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Coche"},{"p":"<Unnamed>","l":"Garaxe"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
